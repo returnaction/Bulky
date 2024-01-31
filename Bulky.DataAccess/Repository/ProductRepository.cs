62a8653f07365a9c1d82243b5ccbc127a9442b05
returnaction <obergannv@gmail.com>
2024-01-31 16:25:15 -0500

    Work but now i need to move from customer to admin

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccessData;
+using Bulky.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
---
+++ BulkyWeb/Areas/Customer/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product was created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null || id == 0)
+                return NotFound();
+
+            Product? product = _unitOfWork.Product.Get(p => p.Id == id);
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id == 0)
+                return NotFound();
+
+            Product? product = _unitOfWork.Product.Get(p => p.Id == id);
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? product = _unitOfWork.Product.Get(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product was deleted successfully";
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
